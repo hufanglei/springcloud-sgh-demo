a135dfd7bb83ea98eec2e436f03210f2b693ca56
hfl <690328661@qq.com>
2020-04-04 15:10:48 +0800

    ribbon负载规则替换

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 
 @RestController
+@Slf4j
 public class OrderController {
 
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
@@ -23,7 +26,18 @@ public class OrderController {
      }
 
      @GetMapping("/consumer/payment/get/{id}")
-     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
-         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
-     }
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getforEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            log.info(entity.getHeaders()+"\t"+entity.getBody());
+            return entity.getBody();
+        }else{
+            return new CommonResult<>(444, "操作失败");
+        }
+    }
 }
