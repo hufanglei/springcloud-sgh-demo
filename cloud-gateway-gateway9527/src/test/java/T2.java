73732f0a49bd42f4edeec1f8a8601bad861f99f7
hfl <690328661@qq.com>
2020-04-07 10:14:16 +0800

    72_GateWay常用的Predicate

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,9 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+        //2020-04-07T09:56:37.079+08:00[Asia/Shanghai]
+    }
+}
